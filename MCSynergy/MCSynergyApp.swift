6cc16461bf24cacfc2b4c147092f38244562f88c
Josian van Efferen <github@Josian.nl>
2023-05-17 13:53:53 +0200

    Initial Commit

---
+++ MCSynergy/MCSynergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCSynergyApp.swift
+//  MCSynergy
+//
+//  Created by Josian van Efferen on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MCSynergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
